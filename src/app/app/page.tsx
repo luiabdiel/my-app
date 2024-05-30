c45f5284258be001b41ca4b214c38504870eb491
luiabdiel <luiggiabdieldev@gmail.com>
2024-05-30 15:06:10 -0300

    feat: middleware auth

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,3 +1,7 @@
-export default function Page() {
-  return <h1>App Page</h1>
+import { auth } from "@/service/auth"
+
+export default async function Page() {
+  const session = await auth()
+
+  return <pre>{JSON.stringify(session?.user, null, 1)}</pre>
 }
